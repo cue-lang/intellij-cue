16ed68afa3ddc464111938f14eb746bb610666bd
Joachim Ansorg <github@ja-dev.eu>
2025-09-22 09:50:49 +0200

    chore: switch to platform properties to help with testing, local development and the use of the current gradle-intellij-platform plugin version
    Signed-off-by: Joachim Ansorg <github@ja-dev.eu>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "intellij-cue"
+
+pluginManagement {
+    val platformVersion = (extra["platformVersion"] as String).toInt()
+    plugins {
+        kotlin("jvm") version when {
+            platformVersion >= 252 -> "2.2.0"
+            platformVersion == 251 -> "2.1.10"
+            else -> "2.0.21"
+        }
+    }
+}
